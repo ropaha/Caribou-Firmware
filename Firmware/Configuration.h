8603a94a7db60a165998d7dfb72ab24f5aac8c9f
3d-gussner <3d.gussner@gmail.com>
2019-12-30 10:57:00 +0100

    Update build number

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -17,7 +17,7 @@ extern PGM_P sPrinterName;
 
 // Firmware version
 #define FW_VERSION "3.8.1"
-#define FW_COMMIT_NR   2873
+#define FW_COMMIT_NR   2874
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
