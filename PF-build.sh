09a51151ef12260d298f6b6b0d19394c0fc15041
3d-gussner <3d.gussner@gmail.com>
2020-02-07 12:02:11 +0100

    Updated PF-build.sh documentation

--- PF-build.sh
+++ PF-build.sh
@@ -120,6 +120,8 @@
 # 15 Dec 2019, 3d-gussner, Prepare for switch to Prusa3d/PF-build-env repository
 # 15 Dec 2019, 3d-gussner, Fix Audrino user preferences for the chosen board.
 # 17 Dec 2019, 3d-gussner, Fix "timer0_fract = 0" warning by using Arduino_boards v1.0.3
+# 07 Feb 2019, 3d-gussner, Added branding_Zaribo.sh and debranding_Zaribo.sh scripts to keep firmware as close as possible to origin
+#                          this should help with merge issues we had in the past.
 #### Start check if OSTYPE is supported
 OS_FOUND=$( command -v uname)
 
