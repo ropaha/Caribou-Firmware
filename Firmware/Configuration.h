81fe437fd44b0e583a70fdab14c8ee456c60ba9f
3d-gussner <3d.gussner@gmail.com>
2020-05-02 18:41:34 +0200

    Add git hash

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -18,7 +18,7 @@ extern PGM_P sPrinterName;
 // Firmware version
 #define FW_VERSION "3.9.0-RC3"
 #define FW_COMMIT_NR   4119
-#define FW_COMMIT_HASH "Unknown"
+#define FW_COMMIT_HASH "22b2b211"
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
