f20d16a6b21ad3cede55632d151baae43daa21ef
3d-gussner <3d.gussner@gmail.com>
2020-06-01 12:07:37 +0200

    Bump up commit

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -17,7 +17,7 @@ extern PGM_P sPrinterName;
 
 // Firmware version
 #define FW_VERSION "3.9.0"
-#define FW_COMMIT_NR   4153
+#define FW_COMMIT_NR   4155
 #define FW_COMMIT_HASH ""
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
